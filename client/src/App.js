75461bf496f3989bf3ff6885d0086c607358b3f0
Mohammed <najjarmedd@gmail.com>
2022-10-23 14:03:28 +0100

    add my first version

---
+++ client/src/App.js
@@ -0,0 +1,110 @@
+
+import './App.css';
+import { useState } from 'react';
+import { ButtonBase, Grid } from '@mui/material';
+import { suitekitchen, sk, sk2, sk3, sk4, sk5, sk6, sk7, wtsap } from './assets'
+import Zoom from 'react-medium-image-zoom';
+import 'react-medium-image-zoom/dist/styles.css';
+
+
+
+function App() {
+  // const classes = useStyles();
+  // const img = [sk, sk2, sk3, sk4, sk5, sk6, sk7]
+  const [selected, setSelected] = useState(sk)
+
+  return (
+    <Grid>
+      <div className="App">
+        <header className="App-header">
+          <h2>Suite Kitchen</h2>
+          <img src={suitekitchen} className="App-logo" alt="logo" />
+        </header>
+      </div>
+      <Grid container sx={{ display: "flex", justifyContent: "center", mx: "auto", alignItems: "center", textAlign: "center" }} >
+        <Grid item sx={{ width: "70%" }}>
+          <ButtonBase >
+            <Zoom>
+              <img style={{ width: "100%", border: selected ? "2px solid orange" : "none" }}
+                alt="complex"
+                src={selected}
+                onClick={() => setSelected(selected)}
+              />
+            </Zoom>
+          </ButtonBase>
+        </Grid>
+        <Grid item sx={{ display: "flex", flexDirection: "row", justifyContent: "space-evenly", width: "70%", marginTop: 2 }}>
+          <ButtonBase>
+            <img style={{ width: "80%", height: "auto", border: selected === sk ? "2px solid orange" : "none" }}
+              alt="complex"
+              src={sk}
+              onClick={() => setSelected(sk)}
+            />
+          </ButtonBase>
+          <ButtonBase>
+            <img style={{ width: "80%", height: "auto", border: selected === sk2 ? "2px solid orange" : "none" }}
+              alt="complex"
+              src={sk2}
+              onClick={() => setSelected(sk2)}
+            />
+          </ButtonBase>
+          <ButtonBase>
+            <img style={{ width: "80%", height: "auto", border: selected === sk3 ? "2px solid orange" : "none" }}
+              alt="complex"
+              src={sk3}
+              onClick={() => setSelected(sk3)}
+            />
+          </ButtonBase>
+          <ButtonBase>
+            <img style={{ width: "80%", height: "auto", border: selected === sk4 ? "2px solid orange" : "none" }}
+              alt="complex"
+              src={sk4}
+              onClick={() => setSelected(sk4)}
+            />
+          </ButtonBase>
+          <ButtonBase>
+            <img style={{ width: "80%", height: "auto", border: selected === sk5 ? "2px solid orange" : "none" }}
+              alt="complex"
+              src={sk5}
+              onClick={() => setSelected(sk5)}
+            />
+          </ButtonBase>
+          <ButtonBase>
+            <img style={{ width: "80%", height: "auto", border: selected === sk6 ? "2px solid orange" : "none" }}
+              alt="complex"
+              src={sk6}
+              onClick={() => setSelected(sk6)}
+            />
+          </ButtonBase>
+          <ButtonBase>
+            <img style={{ width: "80%", height: "auto", border: selected === sk7 ? "2px solid orange" : "none" }}
+              alt="complex"
+              src={sk7}
+              onClick={() => setSelected(sk7)}
+            />
+          </ButtonBase>
+
+        </Grid>
+      </Grid>
+      <div className='App'>
+
+        <h2 style={{ color: "#ffffff" }}>
+          تصميم و تركيب المطابخ العصرية لذى سويت كتشن
+        </h2>
+        <a
+          className="App-link"
+          href="https://wa.link/t0ys0v"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={wtsap} alt="logo" />
+        </a>
+        <p style={{ color: "#ffffff" }}>All Rights Reserved © 2022</p>
+      </div>
+
+
+    </Grid>
+  );
+}
+
+export default App;
